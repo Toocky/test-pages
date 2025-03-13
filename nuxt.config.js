625c7fd1d126db3c1d8ff94720e7668b04b60dfb
Toocky <115723035+Toocky@users.noreply.github.com>
2025-03-13 16:46:57 +0000

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // Source directory
+  srcDir: './',
+  
   // Target: static site generation
   ssr: true,
 
