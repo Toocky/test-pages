b5c2bcf32e83e04dbed5898f231449b99f04ab98
Toocky <115723035+Toocky@users.noreply.github.com>
2025-03-13 22:39:27 +0000

    update

---
+++ interfaces/model.ts
@@ -0,0 +1,24 @@
+export interface IStatusInfo {
+  dt: Date;
+  status: string;
+}
+
+export interface IModelAvailabilityRow {
+  model: string;
+  provider: string;
+  type: string;
+  avgCost: string | null;
+  inputCost: string | null;
+  outputCost: string | null;
+  maxTokens: number | null;
+  maxResponseTokens: number | null;
+  latencies: (number | null)[];
+  latenciesFormatted: string[];
+  latenciesRating: number[];
+  states: (number | null)[];
+  isExpanded: boolean;
+  totalTracked: number;
+  upTime: number | null;
+  upTimePercentage: number | null;
+  lastStatus?: IStatusInfo;
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
     classSuffix: '',
     dataValue: 'dark',
     forcedTheme: 'dark',
+    classPrefix: '',
   },
 
   icon: {
